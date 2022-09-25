f80c99c8a6fd7b85d618b8564f4e96d079de7526
Abhinav Kant <abhinav.kant@gmail.com>
2022-09-25 20:55:32 +0530

    Added MediaRecorder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Component, ElementRef, Inject, PLATFORM_ID, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,29 +11,40 @@ export class AppComponent {
   @ViewChild('video', { static: true })
   video!: ElementRef<HTMLVideoElement>;
 
-  constructor(@Inject(PLATFORM_ID) private _platform: Object) {}
+  mediaRecorder!: MediaRecorder;
+  parts: Array<Blob>;
+  private onDataAvailableEvent = (ev: BlobEvent) => this.handleDataAvailable(ev);
 
-  onStart() {
-    if(isPlatformBrowser(this._platform) && 'mediaDevices' in navigator) {
-      navigator.mediaDevices.getUserMedia({video: true}).then((ms: MediaStream) => {
-        const _video = this.video.nativeElement;
-        _video.srcObject = ms;
-        _video.play();
-      });
+  constructor(@Inject(PLATFORM_ID) private _platform: Object) {
+    this.parts = new Array<Blob>();
+
+    if (isPlatformBrowser(this._platform) && 'mediaDevices' in navigator) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then((ms: MediaStream) => {
+          const _video = this.video.nativeElement;
+          _video.srcObject = ms;
+          _video.play();
+          this.mediaRecorder = new MediaRecorder(ms);
+        });
     }
   }
 
+  onStart() {
+    this.mediaRecorder.ondataavailable = this.onDataAvailableEvent;
+    this.mediaRecorder.start(1000);
+  }
+
   onStop() {
-    this.video.nativeElement.pause();
-    (this.video.nativeElement.srcObject as MediaStream).getVideoTracks()[0].stop();
-    this.video.nativeElement.srcObject = null;
+    this.mediaRecorder.stop();
+    this.parts = new Array<Blob>();
+  }
+
+  handleDataAvailable(e: BlobEvent) {
+    this.parts.push(e.data);
+    console.log(e);
   }
 
   ngOnDestroy() {
     (this.video.nativeElement.srcObject as MediaStream).getVideoTracks()[0].stop();
   }
-
-
-  title = 'Recorder App';
-  isCameraExist: any;
 }
